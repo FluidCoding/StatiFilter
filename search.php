bba128a2f762c8da512cc61c67b8e93e567c5cd2
FluidCoding <sonnie130@gmail.com>
2014-04-02 01:50:50 -0400

    Cleaned code, added some functions
    has ability to cache games to Matches directory.

--- search.php
+++ search.php
@@ -1,49 +1,65 @@
 <?php
-//$gameMode = {"All Pick", ""}
-$api_key = "52BA3796AFE214AE81E250357A12791A";
+require 'vars.php';
 $match_id_Url = "http://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=";
-$api_key_url = "&key=52BA3796AFE214AE81E250357A12791A";
-//$match = [];
-//$username = ""
-/*
+/**
 	Lookup match data from the match id
 */
 function lookupMatch($match_id){
 	global $match_id_Url, $api_key_url;
-	echo ("Match ID: " . $match_id . "<br>");
-	$ch = curl_init();
+	$file_path = "Matches/".$match_id.".json";
+	$results = "";
+	$match_data = "";
+	
+	// Check if game data is cached already
+	if(file_exists($file_path)){
+		$fp = fopen($file_path, "r");
+		$results = fread($fp, filesize($file_path));
+		$match_data = json_decode($results);
+		printMatch($match_data);
+		fclose($fp);
+	}
+	else{
+		// Get game data from valve
+		$ch = curl_init();
+		$url = $match_id_Url . $match_id . $api_key_url;
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
-	//$ch = curl_init("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?format=XML?match_id=".$match_id."&key=52BA3796AFE214AE81E250357A12791A");
-//	$ch = curl_init("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?match_id=582608489&key=52BA3796AFE214AE81E250357A12791A");
-
-
-//	$fp = fopen("Matches/".$m_id.".json", "w");
-//	curl_setopt($ch, CURLOPT_FILE, $fp);
-	//curl_setopt($ch, CURLOPT_URL, "http://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=582374635&key=52BA3796AFE214AE81E250357A12791A");
-	$url = $match_id_Url . $match_id . $api_key_url;
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-	$result = curl_exec($ch);
-	$match_data = json_decode($result);
-	printMatch($match_data);
-	curl_close($ch);
-//	fclose($fp);
-//	return $match_data;
-
+		// Do work with results
+		$results = curl_exec($ch);
+		$match_data = json_decode($results);
+		printMatch($match_data);
+		curl_close($ch);
+		// Cache game data
+		saveMatch($results, $match_id);
+	}
 }
+
+/**
+	Save Match to file
+*/
+function saveMatch($mData, $m_id){
+	if(!file_exists("Matches/".$m_id.".json")){
+		$fp = fopen("Matches/".$m_id.".json", "w");
+		fwrite($fp, $mData);
+		fclose($fp);
+	}
+}
+
+/**
+	Print Match to webpage
+*/
 function printMatch($mData){
 	$eStr = "";
 	echo "<div id='match'>";
-//	print_r($mData);
+
 	$winner = "Radiant";
-	if($mData->result->radiant_win == "false"){
+	if($mData->result->radiant_win == false){
 		$winner = "Dire";
 	}
 
 	echo "<p> Winner: ". $winner . "<br>";
-
 
 	echo "<div id='teams'>";
 		echo "<table>
@@ -145,87 +161,18 @@ function printMatch($mData){
 */
       		echo "</tr>";
       		}
-      		echo "</tbody></table>";
-		
-
-	// Loop Through PLayers
-      	/*
-	for($i=0; $i<5;++$i){
-		echo($mData->result->players[$i]->account_id . " " ); 
-		echo($mData->result->players[$i]->hero_id . " ");
-		echo($mData->result->players[$i]->kills . " ");
-		echo($mData->result->players[$i]->deaths . " ");
-		echo($mData->result->players[$i]->assists . " ");
-		echo($mData->result->players[$i]->gold . " ");
-		echo($mData->result->players[$i]->last_hits . " ");
-		echo($mData->result->players[$i]->denies . " ");
-		echo($mData->result->players[$i]->gold_per_min . " ");
-		echo($mData->result->players[$i]->xp_per_min . " ");
-		echo($mData->result->players[$i]->gold_spent . " ");
-		echo($mData->result->players[$i]->hero_damage . " ");
-		echo($mData->result->players[$i]->tower_damage . " ");
-		echo($mData->result->players[$i]->hero_healing . " ");
-		echo($mData->result->players[$i]->level . " ");
-
-		echo($mData->result->players[$i]->item_0 . " ");
-		echo($mData->result->players[$i]->item_1 . " ");
-		echo($mData->result->players[$i]->item_2 . " ");
-		echo($mData->result->players[$i]->item_3 . " ");
-		echo($mData->result->players[$i]->item_4 . " ");
-		echo($mData->result->players[$i]->item_5 . " ");
-		echo "<br>";
-
-	}
-	// Dire Team
-	echo ("<br><h2 id='dr'>Dire Team </h2><br>");
-	for($i=5; $i<10;++$i){
-		echo($mData->result->players[$i]->account_id . " " ); 
-		echo($mData->result->players[$i]->hero_id . " ");
-		echo($mData->result->players[$i]->kills . " ");
-		echo($mData->result->players[$i]->deaths . " ");
-		echo($mData->result->players[$i]->assists . " ");
-		echo($mData->result->players[$i]->gold . " ");
-		echo($mData->result->players[$i]->last_hits . " ");
-		echo($mData->result->players[$i]->denies . " ");
-		echo($mData->result->players[$i]->gold_per_min . " ");
-		echo($mData->result->players[$i]->xp_per_min . " ");
-		echo($mData->result->players[$i]->gold_spent . " ");
-		echo($mData->result->players[$i]->hero_damage . " ");
-		echo($mData->result->players[$i]->tower_damage . " ");
-		echo($mData->result->players[$i]->hero_healing . " ");
-		echo($mData->result->players[$i]->level . " ");
-
-		echo($mData->result->players[$i]->item_0 . " ");
-		echo($mData->result->players[$i]->item_1 . " ");
-		echo($mData->result->players[$i]->item_2 . " ");
-		echo($mData->result->players[$i]->item_3 . " ");
-		echo($mData->result->players[$i]->item_4 . " ");
-		echo($mData->result->players[$i]->item_5 . " ");
-
-		echo "<br>";
-
-	}
-//	foreach $mData->result->players[account_id as $v {
-//		echo($v . "<br>");
-	*/
+   		echo "</tbody></table>";
 	echo "</div>";
 	echo "</div>";
 }
 
-//$m_id = 582280690;
-//$ch = curl_init("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?format=XML?match_id=".$m_id."&key=52BA3796AFE214AE81E250357A12791A");
-
-//$fp = fopen("GetMatchHistory/".$m_id.".xml", "w");
+// Do some sanitation on input
+// If web request is post 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-	$m_id = $_POST["username"];
+	$m_id = $_POST["match"];
 	lookupMatch($m_id);
 }
-
-
-
-
-//fclose($fp);
 
 ?>
 <!DOCTYPE html>
